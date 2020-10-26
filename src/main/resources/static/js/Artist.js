fb5da64079a0a76ff2ed3b0badabc57e4930fb21
Mohammad Rashid <MRashid1@qa.com>
2020-10-26 15:57:37 +0000

    Added Sidebar animation functions in Artist.js

--- src/main/resources/static/js/Artist.js
+++ src/main/resources/static/js/Artist.js
@@ -226,3 +226,17 @@ function addArtist(name) {
       console.log("Request failed", error);
     });
 }
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("artistTitle").style.marginLeft = "250px";
+  
+
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("artistTitle").style.marginLeft = "0";
+}
