decf164c1657f435cd765178a91a31a2e31b33af
Tay-Dzonu-QA <tdzonu@qa.com>
2020-10-27 10:37:12 +0000

    added repos to select by album and genres

--- src/main/java/com/qa/choonz/persistence/repository/AlbumRepository.java
+++ src/main/java/com/qa/choonz/persistence/repository/AlbumRepository.java
@@ -1,11 +1,20 @@
 package com.qa.choonz.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.qa.choonz.persistence.domain.Album;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
+	
+	@Query(value ="SELECT * FROM album Where artist_id=?1", nativeQuery = true)
+    List<Album> readArtist(Long id);
+	
+	@Query(value ="SELECT * FROM album Where genre_id=?1", nativeQuery = true)
+    List<Album> readGenre(Long id);
 
 }
