72147c5df072c659e3d017571b964ae3077f0157
Tay-Dzonu-QA <tdzonu@qa.com>
2020-10-27 15:49:40 +0000

    incorporated user in tracks

--- src/main/resources/static/js/Tracks.js
+++ src/main/resources/static/js/Tracks.js
@@ -1,28 +1,32 @@
 const params = new URLSearchParams(window.location.search);
 let loggedIn = false;
-let tracksToView="";
-console.log(params);
+let tracksToView = "";
 let singleTrack = false;
+let user = 0;
+console.log(params);
 
 for (const param of params) {
-  if(param[0]==="loggedIn"){
-    loggedIn = param[1];
-  } else if(param[0]==="albums"){
+  if (param[0] === "user") {
+    user = param[1];
+    if (user !== 0) {
+      loggedIn = true;
+    }
+  } else if (param[0] === "albums") {
     let artistOrGenre = param[0];
     let artistOrGenreId = param[1];
-    tracksToView = "/"+artistOrGenre+"/"+artistOrGenreId;
-  } else if(param[0]="tracks"){
-    singleTrack=true;
-    tracksToView = "/"+param[1];
+    tracksToView = "/" + artistOrGenre + "/" + artistOrGenreId;
+  } else if ((param[0] = "tracks")) {
+    singleTrack = true;
+    tracksToView = "/" + param[1];
   }
 }
 
 console.log(tracksToView);
 console.log(loggedIn);
-getTracks(loggedIn,tracksToView,singleTrack);
+getTracks(loggedIn, tracksToView, singleTrack, user);
 
-function getTracks(loggedIn,tracksToView, singleTrack) {
-  fetch("http://localhost:8082/tracks/read"+tracksToView)
+function getTracks(loggedIn, tracksToView, singleTrack, user) {
+  fetch("http://localhost:8082/tracks/read" + tracksToView)
     .then(function (response) {
       if (response.status !== 200) {
         console.log(
@@ -35,18 +39,17 @@ function getTracks(loggedIn,tracksToView, singleTrack) {
         console.log(TrackData);
         let table = document.querySelector("#TrackTable");
         let data;
-        if(singleTrack ===true){
-          data = Object.keys(TrackData)
+        if (singleTrack === true) {
+          data = Object.keys(TrackData);
           TrackData = [TrackData];
-        } else{
+        } else {
           data = Object.keys(TrackData[0]);
         }
         generateTableHead(table, data, loggedIn);
-        generateTable(table, TrackData, loggedIn);
-        if(loggedIn){
-            generateAddTrackBtn(table);
+        generateTable(table, TrackData, loggedIn, user);
+        if (loggedIn) {
+          generateAddTrackBtn(table);
         }
-        
       });
     })
     .catch(function (err) {
@@ -68,6 +71,11 @@ function generateTableHead(table, data, loggedIn) {
   th.appendChild(text);
   row.appendChild(th);
   if (loggedIn) {
+    let th4 = document.createElement("th");
+    let text4 = document.createTextNode("Add to Playlist");
+    th4.appendChild(text4);
+    row.appendChild(th4);
+
     let th2 = document.createElement("th");
     let text2 = document.createTextNode("Edit");
     th2.appendChild(text2);
@@ -80,14 +88,14 @@ function generateTableHead(table, data, loggedIn) {
   }
 }
 
-function generateTable(table, TrackData, loggedIn) {
+function generateTable(table, TrackData, loggedIn, user) {
   for (let element of TrackData) {
     let row = table.insertRow();
     for (key in element) {
       let cell = row.insertCell();
       let text = document.createTextNode(element[key]);
-      if(key ==="album"){
-        text =  document.createTextNode(element[key].name)
+      if (key === "album") {
+        text = document.createTextNode(element[key].name);
       }
       cell.appendChild(text);
     }
@@ -95,133 +103,183 @@ function generateTable(table, TrackData, loggedIn) {
     let myViewButton = document.createElement("button");
     myViewButton.className = "btn";
     myViewButton.id = "ViewTrackButton";
-    myViewButton.onclick = function(){document.location='Track.html?loggedIn='+loggedIn+'&tracks='+element.id};
+    myViewButton.onclick = function () {
+      document.location = "Track.html?user=" + user + "&tracks=" + element.id;
+    };
 
     let viewIcon = document.createElement("span");
     viewIcon.className = "material-icons";
-    viewIcon.innerHTML="launch";
+    viewIcon.innerHTML = "launch";
     myViewButton.appendChild(viewIcon);
     newCell.appendChild(myViewButton);
 
     if (loggedIn) {
-        let newCell2 = row.insertCell();
-        let myEditButton = document.createElement("button");
-        myEditButton.className = "btn";
-        myEditButton.id = "EditTrackButton";
-        myEditButton.setAttribute("data-toggle", "modal");
-        myEditButton.setAttribute("data-target", "#EditTrackModal");
-    
-        let editIcon = document.createElement("span");
-        editIcon.className = "material-icons";
-        editIcon.innerHTML="create";
-        myEditButton.appendChild(editIcon);
-        let ID = element.id;
-        let Name=element.name;
-        let Duration = element.duration;
-        let Lyrics = element.lyrics;
-        myEditButton.onclick = function () {
-          changeEditTrackModal(ID, Name,Duration,Lyrics);
-        };
-        newCell2.appendChild(myEditButton);
+      let ID = element.id;
+      let Name = element.name;
+      let Duration = element.duration;
+      let Lyrics = element.lyrics;
 
-        let newCell3 = row.insertCell();
-        let myDeleteButton = document.createElement("button");
-        myDeleteButton.className = "btn";
-        myDeleteButton.id = "DeleteTrackButton"+element.name;
+      let newCell4 = row.insertCell();
+      let addToPlaylistButton = document.createElement("button");
+      addToPlaylistButton.className = "btn";
+      addToPlaylistButton.id = "addToPlaylistButton" + element.name;
 
-        let deleteIcon = document.createElement("span");
-        deleteIcon.className = "material-icons";
-        deleteIcon.innerHTML="delete";
-        myDeleteButton.appendChild(deleteIcon);
-        myDeleteButton.onclick = function () {
-            deleteTrack(element.id);
-        };
-        newCell3.appendChild(myDeleteButton);
+      let addToPlaylistIcon = document.createElement("span");
+      addToPlaylistIcon.className = "material-icons";
+      addToPlaylistIcon.innerHTML = "add";
+      addToPlaylistButton.appendChild(addToPlaylistIcon);
+      addToPlaylistButton.onclick = function () {
+        changeaddToPlaylistModal(ID, Name, user);
+      };
+      newCell4.appendChild(addToPlaylistButton);
+
+      let newCell2 = row.insertCell();
+      let myEditButton = document.createElement("button");
+      myEditButton.className = "btn";
+      myEditButton.id = "EditTrackButton";
+      myEditButton.setAttribute("data-toggle", "modal");
+      myEditButton.setAttribute("data-target", "#EditTrackModal");
+
+      let editIcon = document.createElement("span");
+      editIcon.className = "material-icons";
+      editIcon.innerHTML = "create";
+      myEditButton.appendChild(editIcon);
+      myEditButton.onclick = function () {
+        changeEditTrackModal(ID, Name, Duration, Lyrics);
+      };
+      newCell2.appendChild(myEditButton);
+
+      let newCell3 = row.insertCell();
+      let myDeleteButton = document.createElement("button");
+      myDeleteButton.className = "btn";
+      myDeleteButton.id = "DeleteTrackButton" + element.name;
+
+      let deleteIcon = document.createElement("span");
+      deleteIcon.className = "material-icons";
+      deleteIcon.innerHTML = "delete";
+      myDeleteButton.appendChild(deleteIcon);
+      myDeleteButton.onclick = function () {
+        deleteTrack(element.id);
+      };
+      newCell3.appendChild(myDeleteButton);
     }
   }
 }
 
-function generateAddTrackBtn(table){
-    let tableFooter = document.createElement("footer");
-    let myAddTrackButton = document.createElement("button");
-    myAddTrackButton.className = "btn btn-outline-primary";
-    myAddTrackButton.innerHTML = "Add Track";
-    myAddTrackButton.id = "AddTrackButton";
-    myAddTrackButton.setAttribute("data-toggle", "modal");
-    myAddTrackButton.setAttribute("data-target", "#AddTrackModal");
-    myAddTrackButton.onclick = function () {
-      changeAddTrackModal();
-    };
+function generateAddTrackBtn(table) {
+  let tableFooter = document.createElement("footer");
+  let myAddTrackButton = document.createElement("button");
+  myAddTrackButton.className = "btn btn-outline-primary";
+  myAddTrackButton.innerHTML = "Add Track";
+  myAddTrackButton.id = "AddTrackButton";
+  myAddTrackButton.setAttribute("data-toggle", "modal");
+  myAddTrackButton.setAttribute("data-target", "#AddTrackModal");
+  myAddTrackButton.onclick = function () {
+    changeAddTrackModal();
+  };
 
-    tableFooter.appendChild(myAddTrackButton);
-    table.appendChild(tableFooter);
+  tableFooter.appendChild(myAddTrackButton);
+  table.appendChild(tableFooter);
 }
 
 function deleteTrack(id) {
-    fetch("http://localhost:8082/tracks/delete/" + id, {
-      method: "delete",
-      headers: {
-        "Content-type": "application/json",
-      },
+  fetch("http://localhost:8082/tracks/delete/" + id, {
+    method: "delete",
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    // .then(json)
+    .then(function (data) {
+      console.log("Request succeeded with JSON response", data);
+      location.reload();
     })
-      // .then(json)
-      .then(function (data) {
-        console.log("Request succeeded with JSON response", data);
-        location.reload();
-      })
-      .catch(function (error) {
-        console.log("Request failed", error);
-      });
-  }
+    .catch(function (error) {
+      console.log("Request failed", error);
+    });
+}
 
-  let TrackId;
+let TrackId;
 
-  function changeEditTrackModal(id, name,duration,lyrics) {
-    let modalEditTrackName = document.getElementById("EditTrackName");
-    modalEditTrackName.setAttribute("value", name);
+function changeEditTrackModal(id, name, duration, lyrics) {
+  let modalEditTrackName = document.getElementById("EditTrackName");
+  modalEditTrackName.setAttribute("value", name);
 
-    let modalEditTrackDuration = document.getElementById("EditTrackDuration");
-    modalEditTrackDuration.setAttribute("value", duration);
-    let modalEditTrackLyrics = document.getElementById("EditTrackLyrics");
-    modalEditTrackLyrics.setAttribute("value", lyrics);
+  let modalEditTrackDuration = document.getElementById("EditTrackDuration");
+  modalEditTrackDuration.setAttribute("value", duration);
+  let modalEditTrackLyrics = document.getElementById("EditTrackLyrics");
+  modalEditTrackLyrics.setAttribute("value", lyrics);
 
-    let modalEditTrackAlbum = document.getElementById("EditTrackAlbum");
-    addAlbumList(modalEditTrackAlbum);
-    TrackId = id;
-  }
-  function changeAddTrackModal() {
-    let modalEditTrackAlbum = document.getElementById("TrackAlbum");
-    addAlbumList(modalEditTrackAlbum);
-  }
+  let modalEditTrackAlbum = document.getElementById("EditTrackAlbum");
+  addAlbumList(modalEditTrackAlbum);
+  TrackId = id;
+}
+function changeAddToPlaylistModal(id, name, user) {
+  let modalEditTrackName = document.getElementById("PlaylistAddTrack");
+  modalEditTrackName.setAttribute("value", name);
 
-  function addAlbumList(modalAlbumList){
-    fetch("http://localhost:8082/albums/read")
-      .then(function (response) {
-        if (response.status !== 200) {
-          console.log(
-            "Looks like there was a problem. Status Code: " + response.status
-          );
-          return;
+  let modalAddToPlaylist = document.getElementById("PlaylistAdd");
+  addPlaylists(modalAddToPlaylist,user);
+  TrackId = id;
+}
+function changeAddTrackModal() {
+  let modalEditTrackAlbum = document.getElementById("TrackAlbum");
+  addAlbumList(modalEditTrackAlbum);
+}
+
+function addAlbumList(modalAlbumList) {
+  fetch("http://localhost:8082/albums/read")
+    .then(function (response) {
+      if (response.status !== 200) {
+        console.log(
+          "Looks like there was a problem. Status Code: " + response.status
+        );
+        return;
+      }
+      // Examine the text in the response
+      response.json().then(function (AlbumData) {
+        console.log(AlbumData);
+        for (let element of AlbumData) {
+          console.log(element);
+          let albumList = document.createElement("option");
+          albumList.innerHTML = element.id + ". " + element.name;
+          modalAlbumList.appendChild(albumList);
         }
-        // Examine the text in the response
-        response.json().then(function (AlbumData) {
-          console.log(AlbumData);
-          for(let element of AlbumData){
-            console.log(element);
-            let albumList = document.createElement("option");
-            albumList.innerHTML = element.id +". "+element.name;
-            modalAlbumList.appendChild(albumList);
-          }
-        });
-      })
-      .catch(function (err) {
-        console.log("Fetch Error :-S", err);
       });
-  }
-  document
+    })
+    .catch(function (err) {
+      console.log("Fetch Error :-S", err);
+    });
+}
+function addPlaylists(modalAddToPlaylist,user) {
+  fetch("http://localhost:8082/playlists/user/"+user)
+    .then(function (response) {
+      if (response.status !== 200) {
+        console.log(
+          "Looks like there was a problem. Status Code: " + response.status
+        );
+        return;
+      }
+      // Examine the text in the response
+      response.json().then(function (PlaylistData) {
+        console.log(PlaylistData);
+        for (let element of PlaylistData) {
+          console.log(element);
+          let playlists = document.createElement("option");
+          playlists.innerHTML = element.id + ". " + element.name;
+          modalAddToPlaylist.appendChild(playlists);
+        }
+      });
+    })
+    .catch(function (err) {
+      console.log("Fetch Error :-S", err);
+    });
+}
+
+document
   .querySelector("form.EditTrack")
   .addEventListener("submit", function (stop) {
-     stop.preventDefault();
+    stop.preventDefault();
 
     let formElements = document.querySelector("form.EditTrack").elements;
     console.log(formElements);
@@ -235,36 +293,42 @@ function deleteTrack(id) {
 
     let TrackId1 = parseInt(TrackId);
     console.log(TrackId1);
-    editTrack(EditTrackName, TrackId1,EditTrackDuration,EditTrackLyrics,TrackAlbumId);
+    editTrack(
+      EditTrackName,
+      TrackId1,
+      EditTrackDuration,
+      EditTrackLyrics,
+      TrackAlbumId
+    );
   });
 
-  function editTrack(name, TrackId, duration,lyrics,album) {
-    fetch("http://localhost:8082/tracks/update/" + TrackId, {
-      method: "put",
-      headers: {
-        "Content-type": "application/json",
+function editTrack(name, TrackId, duration, lyrics, album) {
+  fetch("http://localhost:8082/tracks/update/" + TrackId, {
+    method: "put",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: (json = JSON.stringify({
+      id: TrackId,
+      name: name,
+      duration: duration,
+      lyrics: lyrics,
+      albums: {
+        id: album,
       },
-      body: (json = JSON.stringify({
-        "id": TrackId,
-        "name": name,
-        "duration":duration,
-        "lyrics":lyrics,
-        "albums":{
-          "id":album
-        }
-      })),
+    })),
+  })
+    .then(json)
+    .then(function (data) {
+      console.log("Request succeeded with JSON response", data);
+      location.reload();
     })
-      .then(json)
-      .then(function (data) {
-        console.log("Request succeeded with JSON response", data);
-        location.reload();
-      })
-      .catch(function (error) {
-        console.log("Request failed", error);
-      });
-  }
+    .catch(function (error) {
+      console.log("Request failed", error);
+    });
+}
 
-  document
+document
   .querySelector("form.Track")
   .addEventListener("submit", function (stop) {
     stop.preventDefault();
@@ -277,30 +341,30 @@ function deleteTrack(id) {
     let TrackAlbum = TrackAlbum.split(".");
     let TrackAlbumId = parseInt(TrackAlbum[0]);
 
-    addTrack(AlbumName,TrackDuration,TrackLyrics,TrackAlbumId)
+    addTrack(AlbumName, TrackDuration, TrackLyrics, TrackAlbumId);
   });
 
-  function addTrack(name, duration,lyrics,album) {
-    fetch("http://localhost:8082/tracks/create", {
-      method: "post",
-      headers: {
-        "Content-type": "application/json",
+function addTrack(name, duration, lyrics, album) {
+  fetch("http://localhost:8082/tracks/create", {
+    method: "post",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: (json = JSON.stringify({
+      name: name,
+      duration: duration,
+      lyrics: lyrics,
+      tracks: {
+        id: album,
       },
-      body: (json = JSON.stringify({
-        "name": name,
-        "duration":duration,
-        "lyrics":lyrics,
-        "tracks":{
-          "id":album
-        }
-      })),
+    })),
+  })
+    .then(json)
+    .then(function (data) {
+      console.log("Request succeeded with JSON response", data);
+      location.reload();
     })
-      .then(json)
-      .then(function (data) {
-        console.log("Request succeeded with JSON response", data);
-        location.reload();
-      })
-      .catch(function (error) {
-        console.log("Request failed", error);
-      });
-  }
+    .catch(function (error) {
+      console.log("Request failed", error);
+    });
+}
