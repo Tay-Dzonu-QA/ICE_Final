2fe7768e734537197be6865d43b0f2dd7bb46dcb
Tay-Dzonu-QA <tdzonu@qa.com>
2020-10-28 16:01:35 +0000

    created log in / out buttons

--- src/main/resources/static/js/NavLinks.js
+++ src/main/resources/static/js/NavLinks.js
@@ -6,3 +6,10 @@ let genreLink = document.querySelector("#GenreLink");
 genreLink.href= "Genre.html?user=" + user;
 let trackLink = document.querySelector("#TrackLink");
 trackLink.href= "Track.html?user=" + user;
+
+let indexLink = document.querySelector("#IndexLink");
+if(user==0){
+    indexLink.innerHTML = "Log In";
+}else{
+    indexLink.innerHTML = "Log Out"
+}
