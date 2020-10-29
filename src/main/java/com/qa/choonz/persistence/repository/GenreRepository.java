ba2fbc6186991cbdfeea571886dec922d2cd22cf
Tay-Dzonu-QA <tdzonu@qa.com>
2020-10-29 10:08:43 +0000

    Created repo commands for genre orders

--- src/main/java/com/qa/choonz/persistence/repository/GenreRepository.java
+++ src/main/java/com/qa/choonz/persistence/repository/GenreRepository.java
@@ -1,5 +1,7 @@
 package com.qa.choonz.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,5 +10,14 @@ import com.qa.choonz.persistence.domain.Genre;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
+	
+	@Query(value ="SELECT * FROM genre ORDER BY id DESC", nativeQuery = true)
+    List<Genre> findAllDesc();
+	
+	@Query(value ="SELECT * FROM genre ORDER BY name", nativeQuery = true)
+    List<Genre> orderByName();
+	
+	@Query(value ="SELECT * FROM genre ORDER BY name DESC", nativeQuery = true)
+    List<Genre> orderByNameDesc();
 	
 }
