6d34f064df17a1e07cea1093b2dd798f9dc4f8db
Tay-Dzonu-QA <tdzonu@qa.com>
2020-10-29 09:38:37 +0000

    created sidebar nav for tracks

---
+++ src/main/resources/static/js/SidebarNavTrack.js
@@ -0,0 +1,27 @@
+GenerateSideBarNav(user);
+
+function GenerateSideBarNav(user){
+  fetch("http://localhost:8082/albums/read")
+      .then(function (response) {
+        if (response.status !== 200) {
+          console.log(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
+          return;
+        }
+        // Examine the text in the response
+        response.json().then(function (AlbumData) {
+          let albumSideBar = document.querySelector("#albumList");
+          for(let element of AlbumData){
+            let albumList = document.createElement("a");
+            albumList.innerHTML = element.name;
+            albumList.href= "Track.html?user=" + user + "&albums=" + element.id;
+            albumSideBar.appendChild(albumList);
+          }
+        });
+      })
+      .catch(function (err) {
+        console.log("Fetch Error :-S", err);
+      });
+    
+}
