23cef4b6b7443c18278994048772af18e30c64b0
Mohammad Rashid <MRashid1@qa.com>
2020-10-29 13:28:11 +0000

    Created separate Javascript for params to be used with other js files

---
+++ src/main/resources/static/js/Params.js
@@ -0,0 +1,13 @@
+const params = new URLSearchParams(window.location.search);
+const paramsAlbum = new URLSearchParams(window.location.search);
+let loggedIn = false;
+let loggedInAlbum = false;
+let albumsToView="";
+let artistOrGenre="";
+let albumOrTrack="";
+let tracksToView = "";
+let singleTrack = false;
+let user =0;
+let userAlbum =0;
+
+
