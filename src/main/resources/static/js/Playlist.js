bac9975aa1e1c4c1521323c6cb66535f81e64835
Tay-Dzonu-QA <tdzonu@qa.com>
2020-11-02 12:39:05 +0000

    FE JS for user and playlist pages

--- src/main/resources/static/js/Playlist.js
+++ src/main/resources/static/js/Playlist.js
@@ -1,5 +1,5 @@
-getTracks(loggedIn, playlistToView, user);
-function getTracks(loggedIn, playlistToView, user) {
+getTracks(playlistToView, user);
+function getTracks(playlistToView, user) {
   fetch("http://localhost:8082/playlists/read/" + playlistToView)
     .then(function (response) {
       if (response.status !== 200) {
@@ -11,14 +11,23 @@ function getTracks(loggedIn, playlistToView, user) {
       // Examine the text in the response
       response.json().then(function (PlaylistData) {
         let TrackData = PlaylistData.tracks;
+        let ID = PlaylistData.id;
+        let Name = PlaylistData.name;
+
+        let modalAddTrackPL = document.getElementById("TrackAdd");
+        addTracks(modalAddTrackPL);
 
         let title = document.querySelector("#PLTitle");
-        title.innerHTML = "Playlist: "+PlaylistData.name;
-        
+        title.innerHTML = "Playlist: " + Name;
         let table = document.querySelector("#PlaylistTable");
         let data = Object.keys(TrackData[0]);
+        let footer = document.querySelector("#PLFooter");
+        let tableFooter = document.createElement("footer");
         generateTableHeadPl(table, data);
-        generateTablePl(table, TrackData, user);
+        generateTablePl(table, TrackData, user, playlistToView);
+        let UserPage = false;
+        generateTableFooterPl(tableFooter, ID, Name, user, UserPage);
+        footer.appendChild(tableFooter);
       });
     })
     .catch(function (err) {
@@ -48,7 +57,7 @@ function generateTableHeadPl(table, data) {
   row.appendChild(th4);
 }
 
-function generateTablePl(table, TrackData, user) {
+function generateTablePl(table, TrackData, user, playlistToView) {
   for (let element of TrackData) {
     let row = table.insertRow();
     for (key in element) {
@@ -86,15 +95,72 @@ function generateTablePl(table, TrackData, user) {
     RemoveIcon.innerHTML = "delete";
     myRemoveButton.appendChild(RemoveIcon);
     myRemoveButton.onclick = function () {
-      removeTrack(element.id);
+      removeTrack(element.id, playlistToView);
     };
     newCell3.appendChild(myRemoveButton);
   }
 }
+function generateTableFooterPl(tableFooter, ID, Name, user, UserPage) {
+  if (UserPage === true) {
+    let myViewPLButton = document.createElement("button");
+    myViewPLButton.className = "btn";
+    myViewPLButton.id = "ViewPLButton" + ID;
+    myViewPLButton.onclick = function () {
+      document.location = "Playlist.html?user=" + user + "&playlists=" + ID;
+    };
+    let viewIcon = document.createElement("span");
+    viewIcon.className = "material-icons";
+    viewIcon.innerHTML = "launch";
+    myViewPLButton.appendChild(viewIcon);
+    tableFooter.appendChild(myViewPLButton);
+  }
 
-function removeTrack(TrackId){
-  fetch("http://localhost:8082/playlists/remove/"+playlistToView+"/"+TrackId, {
-    method: "put",
+  let myAddTrackButton = document.createElement("button");
+  myAddTrackButton.className = "btn ";
+  myAddTrackButton.id = "AddTrackButton" + ID;
+  myAddTrackButton.setAttribute("data-toggle", "modal");
+  myAddTrackButton.setAttribute("data-target", "#AddTrackPLModal");
+  myAddTrackButton.onclick = function () {
+    changeAddTrackModal(ID, Name);
+  };
+  let addToPlaylistIcon = document.createElement("span");
+  addToPlaylistIcon.className = "material-icons";
+  addToPlaylistIcon.innerHTML = "add";
+  myAddTrackButton.appendChild(addToPlaylistIcon);
+
+  let myEditButtonPL = document.createElement("button");
+  myEditButtonPL.className = "btn ";
+  myEditButtonPL.id = "EditPLButton" + ID;
+  myEditButtonPL.setAttribute("data-toggle", "modal");
+  myEditButtonPL.setAttribute("data-target", "#EditPLModal");
+  myEditButtonPL.onclick = function () {
+    changeEditPLModal(ID, Name);
+  };
+  let editIcon = document.createElement("span");
+  editIcon.className = "material-icons";
+  editIcon.innerHTML = "create";
+  myEditButtonPL.appendChild(editIcon);
+
+  let myDeleteButtonPL = document.createElement("button");
+  myDeleteButtonPL.className = "btn";
+  myDeleteButtonPL.id = "DeletePLButton" + ID;
+  myDeleteButtonPL.onclick = function () {
+    deletePL(ID);
+  };
+  let deleteIcon = document.createElement("span");
+  deleteIcon.className = "material-icons";
+  deleteIcon.innerHTML = "delete";
+  myDeleteButtonPL.appendChild(deleteIcon);
+
+
+  tableFooter.appendChild(myAddTrackButton);
+  tableFooter.appendChild(myEditButtonPL);
+  tableFooter.appendChild(myDeleteButtonPL);
+}
+
+function deletePL(ID) {
+  fetch("http://localhost:8082/playlists/delete/" + ID, {
+    method: "delete",
     headers: {
       "Content-type": "application/json",
     },
@@ -102,11 +168,130 @@ function removeTrack(TrackId){
     // .then(json)
     .then(function (data) {
       console.log("Request succeeded with JSON response", data);
-      location.reload();
+      // document.location = "User.html?user="+user;
     })
     .catch(function (error) {
       console.log("Request failed", error);
     });
+}
+let PLID;
+function changeEditPLModal(id, name) {
+  let modalPH = document.getElementById("EditPLName");
+  modalPH.setAttribute("value", name);
+  PLID = id;
+}
+document
+  .querySelector("form.EditPL")
+  .addEventListener("submit", function (stop) {
+    stop.preventDefault();
 
+    let formElements = document.querySelector("form.EditPL").elements;
 
+    let EditPLname = formElements["EditPLName"].value;
+
+    editPL(EditPLname, PLID);
+  });
+
+function editPL(name, PLID) {
+  let ID = parseInt(PLID);
+  fetch("http://localhost:8082/playlists/update/" + PLID, {
+    method: "put",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: (json = JSON.stringify({
+      id: ID,
+      name: name,
+    })),
+  })
+    .then(json)
+    .then(function (data) {
+      console.log("Request succeeded with JSON response", data);
+      // location.reload();
+    })
+    .catch(function (error) {
+      console.log("Request failed", error);
+    });
+}
+
+function removeTrack(TrackId, playlistToView) {
+  fetch(
+    "http://localhost:8082/playlists/remove/" + playlistToView + "/" + TrackId,
+    {
+      method: "put",
+      headers: {
+        "Content-type": "application/json",
+      },
+    }
+  )
+    // .then(json)
+    .then(function (data) {
+      console.log("Request succeeded with JSON response", data);
+      // location.reload();
+    })
+    .catch(function (error) {
+      console.log("Request failed", error);
+    });
+}
+
+function changeAddTrackModal(ID, Name){
+  let modalEditTrackName = document.getElementById("AddTrackPL");
+    modalEditTrackName.setAttribute("value", Name);
+    PLID = ID;
+}
+
+function addTracks(modalAddTrackPL) {
+  fetch("http://localhost:8082/tracks/read")
+      .then(function(response) {
+          if (response.status !== 200) {
+              console.log(
+                  "Looks like there was a problem. Status Code: " + response.status
+              );
+              return;
+          }
+          // Examine the text in the response
+          response.json().then(function(TrackData) {
+              for (let element of TrackData) {
+                  let tracks = document.createElement("option");
+                  tracks.innerHTML = element.id + ". " + element.name;
+                  modalAddTrackPL.appendChild(tracks);
+              }
+          });
+      })
+      .catch(function(err) {
+          console.log("Fetch Error :-S", err);
+      });
+}
+
+document
+    .querySelector("form.AddTrackPL")
+    .addEventListener("submit", function(stop) {
+        stop.preventDefault();
+
+        let formElements = document.querySelector("form.AddTrackPL").elements;
+
+        let TrackPlaylist = formElements["TrackAdd"].value;
+        let TrackPlaylist1 = TrackPlaylist.split(".");
+        let TrackId1 = parseInt(TrackPlaylist1[0]);
+        let TrackPlaylistId = parseInt(PLID);
+        addToPlaylist(TrackId1, TrackPlaylistId);
+    });
+
+function addToPlaylist(TrackId1, TrackPlaylistId) {
+    fetch(
+            "http://localhost:8082/playlists/add/" + TrackPlaylistId + "/" + TrackId1, {
+                method: "put",
+                headers: {
+                    "Content-type": "application/json",
+                },
+            }
+        )
+        // .then(json)
+        .then(function(data) {
+            console.log("Request succeeded with JSON response", data);
+            location.reload();
+        })
+        .catch(function(error) {
+            console.log("Request failed", error);
+        });
 }
--- src/main/resources/static/js/User.js
+++ src/main/resources/static/js/User.js
@@ -9,73 +9,45 @@ fetch("http://localhost:8082/users/read/" + user)
     //console.log(response);
     response.json().then(function (UserData) {
       console.log(UserData);
-      let welcome = document.querySelector('#userName');
-      welcome.innerHTML = "Welcome back "+UserData.name;
+      let welcome = document.querySelector("#userName");
+      welcome.innerHTML = "Welcome back " + UserData.name;
       let PLData = UserData.playlists;
       let tables = document.querySelector("#PLTables");
       console.log(PLData);
-        createTables(tables, PLData);
+      createTables(tables, PLData);
     });
   })
   .catch(function (err) {
     console.log("Fetch Error :-S", err);
   });
 
-  function createTables(tables, PLData) {
-    for (element of PLData) {
-      let tableDiv = document.createElement("div");
-      let table = document.createElement("table");
-      let Listname = document.createElement("h2");
-      Listname.textContent = element.name;
-      let data = Object.keys(element.tracks[0]);
-      generateTableHeadPl(table, data);
-      generateTablePl(table, element.tracks,element.name);
-      tableDiv.appendChild(Listname);
-      tableDiv.appendChild(table);
-  
-      //Need to add footer for deleta and edit track list
-      let ID =element.id;
-      let Name = element.name;
-  
-      let tableFooter = document.createElement("footer");
-  
-      let myAddTrackButton = document.createElement("button");
-      myAddTrackButton.className = "btn btn-outline-primary";
-      myAddTrackButton.innerHTML = "Add Track";
-      myAddTrackButton.id = "AddTrackButton"+element.name;
-      myAddTrackButton.setAttribute("data-toggle", "modal");
-      myAddTrackButton.setAttribute("data-target", "#AddTrackModal");
-      myAddTrackButton.onclick = function () {
-        changeAddTrackModal(ID, Name);
-      };
-  
-      let myEditButtonPL = document.createElement("button");
-      myEditButtonPL.className = "btn btn-outline-primary";
-      myEditButtonPL.innerHTML = "Edit";
-      myEditButtonPL.id = "EditPLButton"+element.name;
-      myEditButtonPL.setAttribute("data-toggle", "modal");
-      myEditButtonPL.setAttribute("data-target", "#EditPLModal");
-      myEditButtonPL.onclick = function () {
-        changeEditPLModal(ID, Name);
-      };
-  
-      let myDeleteButtonPL = document.createElement("button");
-      myDeleteButtonPL.className = "btn btn-outline-primary";
-      myDeleteButtonPL.innerHTML = "Delete";
-      myDeleteButtonPL.id = "DeletePLButton"+element.name;
-      myDeleteButtonPL.onclick = function () {
-        deleteTrackList(ID);
-      };
-  
-      tableFooter.appendChild(myAddTrackButton);
-      tableFooter.appendChild(myEditButtonPL);
-      tableFooter.appendChild(myDeleteButtonPL);
-      tableDiv.appendChild(tableFooter);
-  
-      tableDiv.className = "col-lg";
-      tables.appendChild(tableDiv);
-      let seperator = document.createElement("div");
-      seperator.className = "col-1";
-      tables.appendChild(seperator);
-    }
+function createTables(tables, PLData) {
+  for (element of PLData) {
+    let ID = element.id;
+    let Name = element.name;
+    let modalAddTrackPL = document.getElementById("TrackAdd");
+    addTracks(modalAddTrackPL);
+
+    let tableDiv = document.createElement("div");
+    let table = document.createElement("table");
+    let Listname = document.createElement("h2");
+    Listname.textContent = Name;
+    playlistToView = element.id;
+    let data = Object.keys(element.tracks[0]);
+    generateTableHeadPl(table, data);
+    generateTablePl(table, element.tracks, Name, playlistToView);
+    tableDiv.appendChild(Listname);
+    tableDiv.appendChild(table);
+    let tableFooter = document.createElement("footer");
+    let UserPage = true;
+    generateTableFooterPl(tableFooter, ID, Name, user, UserPage);
+    //Need to add footer for deleta and edit track list
+    tableDiv.appendChild(tableFooter);
+
+    tableDiv.className = "col-lg";
+    tables.appendChild(tableDiv);
+    let seperator = document.createElement("div");
+    seperator.className = "col-1";
+    tables.appendChild(seperator);
   }
+}
